7249c8fdce0fbca2f4528dfffc485bf7f33e6e42
abdelrahmanayaad <abdelrahmanayad74@gmail.com>
2022-10-07 21:23:53 +0200

    Libraries

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import IntroSliderScreen from './src/screens/IntroSliderScreen';
 
 function App(props) {
   return (
     <View>
-      <Text>Hello World !</Text>
+      <IntroSliderScreen />
     </View>
   );
 }
---
+++ src/screens/IntroSliderScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function IntroSliderScreen(props) {
+  return (
+    <View>
+      <Text>IntroSliderScreen</Text>
+    </View>
+  );
+}
+
+export default IntroSliderScreen;
